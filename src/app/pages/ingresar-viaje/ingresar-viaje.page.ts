a659421f02b6e072dc086c4b2a4da98a751b8ec0
cristianvernal <cr.vernal@duocuc.cl>
2022-11-22 11:58:45 -0300

    ingresar viaje

--- src/app/pages/ingresar-viaje/ingresar-viaje.page.ts
+++ src/app/pages/ingresar-viaje/ingresar-viaje.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PickerController } from '@ionic/angular';
 
 @Component({
   selector: 'app-ingresar-viaje',
@@ -7,9 +8,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IngresarViajePage implements OnInit {
 
-  constructor() { }
+  constructor(private pickerCtrl: PickerController) { }
 
   ngOnInit() {
   }
+  async openPicker() {
+    const picker = await this.pickerCtrl.create({
+      columns: [
+        {
+          name: 'asientos',
+          options: [
+            {
+              text: '1',
+              value: 'uno',
+            },
+            {
+              text: '2',
+              value: 'dos',
+            },
+            {
+              text: '3',
+              value: 'tres',
+            },
+            {
+              text: '4',
+              value: 'cuatro',
+            },
+          ],
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancelar',
+        },
+        {
+          text: 'Confirmar',
+          handler: (value) => {
+            window.alert(`has seleccionado: ${value.asientos.value}`);
+          },
+        },
+      ],
+    });
 
+    await picker.present();
+  }
 }
+
