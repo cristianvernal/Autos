df3df8f438dac1ffa92fc0d7ea4dd45cdc1fb6ac
cristianvernal <cr.vernal@duocuc.cl>
2022-11-24 16:41:25 -0300

    changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'geoLoc',
+  appId: 'com.example.app',
+  appName: 'teLlevoApp',
   webDir: 'www',
   bundledWebRuntime: false
 };
